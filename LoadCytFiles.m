2ca46a870b8728c1cccf3386b9bf2a496b430eb7
AbrahamAvelar <jabrahamavelar@gmail.com>
2017-03-15 10:56:55 -0600

    Update LoadCytFiles.m
    El cambio es que ahora lee usando el fcsread viejito que viene de aca, siempre que le pongas un "0"  en la llamada a la función:
    https://github.com/springerlab/Flow-Cytometry-Toolkit-Old/blob/master/script/fcsread.m

--- LoadCytFiles.m
+++ LoadCytFiles.m
@@ -1,7 +1,16 @@
-function [BgDataAll] = LoadCytFiles()
-% Descarga los .fcs que esten en esta carpeta en una sola estructura
+function [BgDataAll] = LoadCytFiles(Reader)
+% Descarga los .fcs que esten en la carpeta actual en una sola estructura
+% Reader = 1 usar el lector viejito que sacamos del github de MSpringer lab
+% si no dice nada, entonces usa el fca_readfcs que es que que ya usabamos
+
 tubos=dir('*.fcs*'); %Nombre del plato que se quiera extraer
-   
+
+if nargin<1 
+    Reader=1;
+end
+
+
+
 for i=1:length(tubos)
    % i/length(tubos) %para saber cuanto lleva
     
@@ -11,22 +20,23 @@ for i=1:length(tubos)
 %         campos = fieldnames(info);
 %         FieldsToDelete = campos([1 3 9 13 15 16 17:end])
 %     end
-    
-    [datos info] = fca_readfcs(tubos(i).name); %aunque datosEscalados no se guarda porque no sabemos ni qu'e con ellos todav'ia
-    if ~isempty(datos)%porque salio un archivo que "no es de tipo fcs soportado" whatever that means
-        %info=rmfield(info, FieldsToDelete);
-        BgDataAll(i).dat = datos;
-        BgDataAll(i).info = info;
+    if Reader
+        [datos info] = fca_readfcs(tubos(i).name); %aunque datosEscalados no se guarda porque no sabemos ni qu'e con ellos todav'ia
+        if ~isempty(datos)%porque salio un archivo que "no es de tipo fcs soportado" whatever that means
+            %info=rmfield(info, FieldsToDelete);
+            BgDataAll(i).dat = datos;
+            BgDataAll(i).info = info;
+        else
+            BgDataAll(i).info.filename=0;
+        end
     else
-    	BgDataAll(i).info.filename=0;
+        [datos, info, metad]=fcsread(tubos(i).name);
+        BgDataAll(i).dat = datos(1:size(datos),:);
+        BgDataAll(i).info= info;
+        BgDataAll(i).meta= metad;
     end
 
 end
 
 end
-
-
-
-
-
 
