78284153abf855b2ecd7d40262ad15599ee7ba60
AbrahamAvelar <jabrahamavelar@gmail.com>
2021-04-10 10:57:33 -0500

    Add files via upload

--- calculateExpFit.m
+++ calculateExpFit.m
@@ -4,10 +4,10 @@ function data = calculateExpFit(data, platos, lecturas)
     for  i = platos
         tosave_a=[];
         tosave_b=[];
-        for w=length( data(i).vivas )
+        for w=1:length( data(i).vivas )
             temp = fit(data(i).t(lecturas)', data(i).vivas(lecturas,w), 'exp1');
-            tosave_a = [tosave_a temp.a];
-            tosave_b = [tosave_b temp.b];
+            tosave_a = [tosave_a; temp.a];
+            tosave_b = [tosave_b; temp.b];
         end
         data(i).fit_a=tosave_a;
         data(i).deathRate=tosave_b;
